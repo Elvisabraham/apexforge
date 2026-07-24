6fba87f160266ccaf7d5cacb02e448dba0b399cd
Elvisabraham <109439739+Elvisabraham@users.noreply.github.com>
2026-07-24 08:59:08 +0100

    Update SolanaProvider.jsx

--- src/components/SolanaProvider.jsx
+++ src/components/SolanaProvider.jsx
@@ -2,16 +2,18 @@ import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
-// The Wallet Standard auto-detects other wallets like Trust, Backpack, and Coinbase!
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function SolanaProvider({ children }) {
   const network = WalletAdapterNetwork.Mainnet;
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // Uses a free RPC endpoint to prevent 403 Forbidden errors
+  const endpoint = useMemo(() => {
+    return 'https://rpc.ankr.com/solana';
+  }, []);
 
   const wallets = useMemo(
     () => [
