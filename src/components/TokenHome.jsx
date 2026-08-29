0a9677de8019311235f57e7eb13596b924c839e4
Elvisabraham <109439739+Elvisabraham@users.noreply.github.com>
2026-08-29 04:34:16 +0100

    Update TokenHome.jsx

--- src/components/TokenHome.jsx
+++ src/components/TokenHome.jsx
@@ -104,6 +104,12 @@ const handleCopyCA = (address) => {
       })
     : baseTokens;
 
+    const SolIcon = ({ className = "w-2.5 h-2.5" }) => (
+  <svg className={className} viewBox="0 0 397 311" fill="currentColor">
+    <path d="M64.6 237.9c2.4-2.4 5.7-3.8 9.2-3.8h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1l62.7-62.7zM64.6 3.8C67 1.4 70.3 0 73.8 0h317.4c5.8 0 8.7 7 4.6 11.1l-62.7 62.7c-2.4 2.4-5.7 3.8-9.2 3.8H6.5c-5.8 0-8.7-7-4.6-11.1L64.6 3.8zM332.4 120.9c-2.4-2.4-5.7-3.8-9.2-3.8H5.8c-5.8 0-8.7 7-4.6 11.1l62.7 62.7c2.4 2.4 5.7 3.8 9.2 3.8h317.4c5.8 0 8.7-7 4.6-11.1l-62.7-62.7z" />
+  </svg>
+);
+
   return (
     <div className="w-full h-full bg-[#0c0d10] text-white grid grid-cols-12 gap-2 p-2 overflow-hidden select-none">
       
